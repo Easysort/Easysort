b85282f1037ba2d38b4279b20b166408f7f1d979
apros7 <lucas.vilsen@gmail.com>
2024-04-16 11:21:42 +0200

    comments

--- src/run.py
+++ src/run.py
@@ -13,16 +13,14 @@ import numpy as np
 
 model = YOLO("/Users/lucasvilsen/Desktop/EasySort/vision/runs/train/weights/best.pt")
 
-# Set the device to GPU if available
 # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 device = torch.device("mps")
 model = model.to(device)
 
-# Open a connection to the camera (0 is usually the default camera)
 cap = cv2.VideoCapture(0)
 window_name = 'DETR Object Detection'
 cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-cv2.resizeWindow(window_name, 800, 600)  # Adjust the size as needed
+cv2.resizeWindow(window_name, 800, 600)  # Adjust here to get correct projected positions
 
 def calculate_farthest_point(box):
     x1, y1, x2, y2 = box.xyxy[0]
@@ -30,18 +28,14 @@ def calculate_farthest_point(box):
     return (x1+x2)//2, (y1+y2)//2
 
 while True:
-    # Capture frame-by-frame
     ret, frame = cap.read()
     if not ret:
         break
 
-    # Create a blank frame to draw the bounding boxes on
     output_frame = frame.copy()
 
-    # Perform object detection on the frame
     results = model(frame, stream=True)
 
-    # Draw bounding boxes on the output frame
     for result in results:
         boxes = result.boxes
         names = model.names
@@ -67,13 +61,10 @@ while True:
             cv2.drawMarker(output_frame, calculate_farthest_point(box), (0, 255, 0), markerType=cv2.MARKER_CROSS, markerSize=10, thickness=2)
             print(f"{names[cls]} at {calculate_farthest_point(box)}")
 
-    # Display the resulting frame
     cv2.imshow(window_name, output_frame)
 
-    # Break the loop if 'q' key is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the camera and close all windows
 cap.release()
 cv2.destroyAllWindows()
