a60cda15331ceb8ebfa0b44885a61f3dde99801d
apros7 <lucas.vilsen@gmail.com>
2024-06-16 20:39:31 +0200

    train changes

--- src/train.py
+++ src/train.py
@@ -4,8 +4,8 @@ import os
 model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
 base_path = "/Users/lucasvilsen/Desktop/EasySort"
-data_yaml = "/Users/lucasvilsen/Desktop/EasySort/vision/recycle.v6i.yolov8/data.yaml"
-save_dir = "/Users/lucasvilsen/Desktop/ReUse/vision/runs"
+data_yaml = "/Users/lucasvilsen/Desktop/EasySort/playground/recycle.v6i.yolov8/data.yaml"
+save_dir = "/Users/lucasvilsen/Desktop/ReUse/runs"
 
 model.train(
     data=data_yaml, 
@@ -17,7 +17,7 @@ model.train(
 )
 
 metrics = model.val()  # evaluate model performance on the validation set
-results = model("/Users/lucasvilsen/Desktop/ReUse/vision/recycle.v6i.yolov8/test/images/2a319efa-pic52_jpg.rf.3731665202ce07c61a72d97142cb8441.jpg")  # predict on an image
+results = model("/Users/lucasvilsen/Desktop/ReUse/playground/recycle.v6i.yolov8/test/images/2a319efa-pic52_jpg.rf.3731665202ce07c61a72d97142cb8441.jpg")  # predict on an image
 
 for result in results:
     boxes = result.boxes  # Boxes object for bounding box outputs
