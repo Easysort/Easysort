6e2ac57b5c3a70f79fd506c3c98e4fa468c90ff8
apros7 <lucas.vilsen@gmail.com>
2024-06-25 12:25:48 +0200

    data engine performance boost, splitting videos in 10 sec chunks and saving everything as frames - delete not working

--- data_engine/data_engine.py
+++ data_engine/data_engine.py
@@ -10,6 +10,7 @@ import cv2
 import time
 import logging
 from datetime import datetime
+import shutil
 
 from utils import get_free_filename
 
@@ -18,7 +19,10 @@ logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s, %(levelname)s]: %
 def get_top_folder(): return subprocess.check_output(["git", "rev-parse", "--show-toplevel"]).decode('utf-8').strip("\n")
 
 class BaseModel():
-    def quit(self): self.cap.release(); cv2.destroyAllWindows()
+    def __init__(self, cap): self.cap = cap
+    def quit(self):
+        if self.cap: self.cap.release()
+        cv2.destroyAllWindows()
 
 class DataRecorder(BaseModel):
     def __init__(self):
@@ -31,7 +35,7 @@ class DataRecorder(BaseModel):
         self.frames_index = 0
         # self.fourcc = cv2.VideoWriter_fourcc(*'avc1')
         # self.fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        super().__init__()
+        super().__init__(self.cap)
 
     def add_description(self, frame): 
         return cv2.rectangle(frame, (10, 10), (50, 30), (0, 0, 255), 2) if self.recording else cv2.rectangle(frame, (10, 10), (50, 30), (128, 128, 128), 2)
@@ -45,7 +49,7 @@ class DataRecorder(BaseModel):
             self.frame_dir = os.path.join(self.top_folder, self.data_folder)
 
     def record_frame(self, frame):
-        cv2.imwrite(os.path.join(self.frame_dir, f"frame_{self.frames_index:06d}.jpg"), frame)
+        cv2.imwrite(os.path.join(self.frame_dir, f"frame_{self.frames_index:04d}.jpg"), frame)
         self.frames_index += 1
 
     def run(self):
@@ -60,30 +64,117 @@ class DataRecorder(BaseModel):
             if key == ord('s'): self.record(start=False)
 
 class DataExplorer(BaseModel):
-    def __init__(self, iterative: bool = False):
+    def __init__(self, data_folder_to_explore: str = "new"):
+        if data_folder_to_explore not in ["new", "verified"]:
+            raise LookupError(f"Data folder to explore has to be one of {["new", "verified"]}, but is {data_folder_to_explore}")
         self.fps = 10
-        self.data_folder = os.path.join(get_top_folder(), "data")
+        self.data_folder = os.path.join(get_top_folder(), "data", data_folder_to_explore)
         self.files = os.listdir(self.data_folder)
         logging.info("\n".join(["You can choose from the following list: ", "--------", 
                                 *[f"{l}: {df}" for l, df in enumerate(self.files)], "--------"]))
-        if iterative:
-            self.iterative_index = int(input("What index to you want to see: "))
-        super().__init__()
+        self.check_new_folder()
+        super().__init__(None)
+
+    def check_new_folder(self):
+        print("Checking 'new' folder for videos too long...")
+        path = os.path.join(get_top_folder(), "data", "new")
+        for folder in os.listdir(path):
+            folder_path = os.path.join(path, folder)
+            files = [f for f in os.listdir(folder_path) if f.startswith('frame_') and f.endswith('.jpg')]
+            files.sort(key=lambda x: int(x.split('_')[1].split('.')[0]))
+
+            num_parts = (len(files) + 99) // 100
+            if num_parts < 2: continue
+            print(num_parts, "for", folder_path)
+            for i in range(num_parts):
+                part_folder = os.path.join(path, f'{folder}_part_{i+1}')
+                os.makedirs(part_folder, exist_ok=True)
+                print("putting", part_folder)
+                for j in range(100):
+                    file_index = i * 100 + j
+                    if file_index < len(files):
+                        file_path = os.path.join(folder_path, files[file_index])
+                        shutil.move(file_path, part_folder)
+            self.delete(folder_path)
+        print("Done checking")
+
+
+    def add_description(self, frame):
+        font = cv2.FONT_HERSHEY_DUPLEX
+        font_scale = 0.5
+        thickness = 1
+
+        cv2.rectangle(frame, (0, 0), (200, 120), (255, 255, 255), -1)
+        overlay = frame.copy()
+        cv2.rectangle(overlay, (0, 0), (200, 120), (0, 0, 0), -1)
+
+        text = "Press 'q' to quit"
+        cv2.putText(frame, text, (10, 20), font, font_scale, (0, 0, 0), thickness)
+
+        text = "Press 'a' to accept"
+        cv2.putText(frame, text, (10, 40), font, font_scale, (0, 0, 0), thickness)
+
+        text = "Press 's' to skip"
+        cv2.putText(frame, text, (10, 60), font, font_scale, (0, 0, 0), thickness)
+
+        text = "Press 'd' to delete"
+        cv2.putText(frame, text, (10, 80), font, font_scale, (0, 0, 0), thickness)
+
+        text = "Press 'r' to review"
+        cv2.putText(frame, text, (10, 100), font, font_scale, (0, 0, 0), thickness)
+
+        return frame
+
+    def prompt_index(self): return int(input("What index to you want to see (put -1 if all): "))
     
-    def view(self, index: int = None):
-        index = self.iterative_index if index is None else index
-        if index is None: raise LookupError("No index was chosen. Set iterative = True when initializing or specify an index when running .view")
-        if index > len(self.files) - 1: logging.warning(f"Your index is {index}, but max is {len(self.files) - 1}, so setting it to 0"); index = 0
+    def accept(self, file_path):
+        verified_dir = 'data/verified'
+        if not os.path.exists(verified_dir):
+            os.makedirs(verified_dir)
+        filename = os.path.basename(file_path)
+        shutil.move(file_path, os.path.join(verified_dir, filename))
+
+    def delete(self, file_path):
+        # Operation not permitted: '/Users/lucasvilsen/Desktop/EasySort/data/new/d_2024-06-25_1'
+        if os.path.exists(file_path):
+            os.remove(file_path)
+
+    def _view(self, index: int = None):
         file_to_view = os.path.join(self.data_folder, self.files[index])
         logging.info(f"Viewing: {file_to_view}")
         frame_files = sorted(os.listdir(file_to_view))
-        print(frame_files)
         for frame_file in frame_files:
             frame_path = os.path.join(file_to_view, frame_file)
             frame = cv2.imread(frame_path)
-            cv2.imshow("frame", frame)
-            if cv2.waitKey(1000//self.fps) & 0xFF == ord('q'): self.quit(); break
+            cv2.imshow("frame", self.add_description(frame.copy()))
+            key = cv2.waitKey(1000//self.fps) & 0xFF
+            if key == ord('q'): self.quit(); break
+            if key == ord('a'): self.accept(file_to_view); return
+            if key == ord('s'): return
+            if key == ord('d'): self.delete(file_to_view); return
+            if key == ord('r'): self._view(index); return
+        
+        while True:
+            cv2.imshow("frame", self.add_description(frame.copy()))
+            key = cv2.waitKey(1000//self.fps) & 0xFF
+            if key == ord('q'): self.quit(); break
+            if key == ord('a'): self.accept(file_to_view); return
+            if key == ord('s'): return
+            if key == ord('d'): self.delete(file_to_view); return
+            if key == ord('r'): self._view(index); return
+    
+    def view(self, index: int = None):
+        index = self.prompt_index() if index is None else index
+        if index > len(self.files) - 1: logging.warning(f"Your index is {index}, but max is {len(self.files) - 1}, so setting it to -1"); index = -1
+        if index >= 0:
+            self._view(index)
+        else:
+            for i in range(len(self.files)):
+                self._view(i)
+
 
 if __name__ == "__main__":
     # DataRecorder().run()
-    DataExplorer(iterative = True).view()
+    DataExplorer().view(3)
+
+# MISSING DELETE
--- data_engine/utils.py
+++ data_engine/utils.py
@@ -1,15 +1,15 @@
 import os
 import datetime
 
-def get_free_filename():
+def get_free_filename(date_str = None):
     """
     Returns a filename for a file that should be followed by the current date,
     but if this is already in the frames folder, then iterate by 1, until
     you have a free file name.
     """
     base_filename = "d"
-    frames_folder = "data"
-    date_str = datetime.date.today().strftime("%Y-%m-%d")
+    frames_folder = "data/new"
+    date_str = datetime.date.today().strftime("%Y-%m-%d") if date_str is None else date_str
     base_filename_with_date = f"{base_filename}_{date_str}"
 
     i = 0
