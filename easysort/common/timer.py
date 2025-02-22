8f89cd6c321156c44829669d7af14c38b4d85bb3
apros7 <lucas.vilsen@gmail.com>
2025-02-22 23:15:29 +0100

    DEBUG=1, timer module

---
+++ easysort/common/timer.py
@@ -0,0 +1,13 @@
+import time, os
+
+def timeit(name):
+    def decorator(f):
+        def wrapper(*args, **kwargs):
+            if int(os.getenv('DEBUG')) > 0:
+                start = time.time()
+                result = f(*args, **kwargs)
+                print(f"{name} took {time.time() - start:.4f} seconds.")
+                return result
+            return f(*args, **kwargs)
+        return wrapper
+    return decorator
