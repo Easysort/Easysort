feb0941cc16b0606e07ea512ec265ea2fb77c89d
apros7 <lucas.vilsen@gmail.com>
2025-11-11 16:06:57 +0100

    fix run2 with new result registry

--- easysort/run2.py
+++ easysort/run2.py
@@ -7,6 +7,9 @@ def run():
     array = np.array([1, 2, 3, 4, 5])
     json_data = {"array": array.tolist()}
     print(path)
-    ResultRegistry.POST(path, "test", "test", json_data)
-    ResultRegistry.POST(path, "test", "test", array)
-    ResultRegistry.POST(path, "test", "test", b"hello")
+    ResultRegistry.POST(path, "test", "test", "json_data", json_data)
+    ResultRegistry.POST(path, "test", "test", "array", array)
+    ResultRegistry.POST(path, "test", "test", "b_hello", b"hello")
+
+if __name__ == "__main__":
+    run()
