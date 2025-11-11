8e7ce943a7d32dc71f72746beb49a84aca558be4
apros7 <lucas.vilsen@gmail.com>
2025-11-11 16:00:31 +0100

    test result registry

---
+++ easysort/run2.py
@@ -0,0 +1,11 @@
+from easysort.registry import ResultRegistry, DataRegistry
+import numpy as np
+import json
+
+def run():
+    path = DataRegistry.LIST("argo")[10]
+    array = np.array([1, 2, 3, 4, 5])
+    json_data = {"array": array.tolist()}
+    ResultRegistry.POST(path, "test", "test", json_data)
+    ResultRegistry.POST(path, "test", "test", array)
+    ResultRegistry.POST(path, "test", "test", b"hello")
