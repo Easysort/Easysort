f01fab9182984906232155701e856d48ebc07a93
apros7 <lucas.vilsen@gmail.com>
2025-11-17 10:59:38 +0100

    check if exists

---
+++ easysort/runners/check_useability.py
@@ -0,0 +1,31 @@
+from easysort.sampler import Sampler, Crop
+from easysort.gpt_trainer import GPTTrainer, YoloTrainer
+from easysort.registry import DataRegistry, ResultRegistry
+
+from tqdm import tqdm
+from typing import List
+from ultralytics.engine.results import Results
+import os
+import datetime
+from easysort.helpers import Sort
+
+## Idea for tracking people:
+## Detect people with gpt-trainer
+## Find out how many unique people
+## Embed image and train classifier/SVM
+
+if __name__ == "__main__":
+    DataRegistry.SYNC()
+    yolo_model = "yolov8s.pt"
+    yolo_trainer = YoloTrainer(yolo_model)
+    gpt_trainer = GPTTrainer()
+    gpt_model = gpt_trainer.model
+    yolo_person_cls_idx = 0
+    project = "argo-people"
+    all_paths = DataRegistry.LIST("argo")
+    data = list(Sort.since(all_paths, datetime.datetime(2025, 11, 10)))
+    data = list(Sort.before(data, datetime.datetime(2025, 11, 17)))
+    print(f"Processing {len(data)} paths out of {len(all_paths)}")
+    for j,path in enumerate(tqdm(data, desc="Processing paths")):
+        if ResultRegistry.EXISTS(path, yolo_model, project): exists += 1
+    print(f"Exists: {exists} / {len(data)}")
