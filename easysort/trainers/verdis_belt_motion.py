9805b7d73df89f87f4be6c63a25ee40805b12950
Lucas Vilsen <lucas.vilsen@gmail.com>
2026-02-06 17:16:09 +0100

    try adding motion trainer

--- easysort/trainers/verdis_belt_motion.py
+++ easysort/trainers/verdis_belt_motion.py
@@ -2,10 +2,20 @@ from easysort.registry import RegistryBase
 from easysort.helpers import REGISTRY_LOCAL_IP
 from easysort.dataloader import DataLoader
 from easysort.trainer import YoloTrainer
-from easysort.validators.verdis_belt import VerdisBeltGroundTruth
 from pathlib import Path
 import cv2
 from uuid import uuid4
+from dataclasses import dataclass, field
+from easysort.helpers import current_timestamp
+
+
+@dataclass
+class VerdisBeltGroundTruth:
+    category: str
+    motion: bool
+    id: str = field(default_factory=lambda: "5adf5d6f-539a-4533-9461-ff8b390fd9cf")
+    metadata: RegistryBase.BaseDefaultTypes.BASEMETADATA = field(default_factory=lambda: RegistryBase.BaseDefaultTypes.BASEMETADATA(model="human", created_at=current_timestamp()))
+
 
 
 def file_to_saved_img_func(registry: RegistryBase, file: Path, expected_save_path: Path):
