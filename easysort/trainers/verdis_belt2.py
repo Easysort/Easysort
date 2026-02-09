a9ca8dff815d448165f52148d9c40fe6d63b8352
Lucas Vilsen <lucas.vilsen@gmail.com>
2026-02-09 17:30:20 +0100

    use registry points in belt trainer too

--- easysort/trainers/verdis_belt2.py
+++ easysort/trainers/verdis_belt2.py
@@ -5,19 +5,23 @@ from easysort.trainer import YoloTrainer
 from pathlib import Path
 import numpy as np
 import cv2
+from easysort.trainers.verdis_belt_motion import VerdisBeltGroundTruth
 
+def file_to_saved_img_func(registry: RegistryBase, file: Path, expected_save_path: Path):
+    cv2.imwrite(expected_save_path / file.name, registry.GET(file, registry.DefaultMarkers.ORIGINAL_MARKER))
 
 if __name__ == "__main__":
     # Train category model
     CATEGORIES = ["plastics", "hard_plastics", "cardboard", "paper", "folie", "empty"]
 
     registry = RegistryBase(base=REGISTRY_LOCAL_IP)
-    dataloader = DataLoader(registry, classes=CATEGORIES, destination=Path("verdis_category_dataset_reworked"))
+    dataloader = DataLoader(registry, classes=CATEGORIES, destination=Path("verdis_category_dataset_reworked2"))
     dataloader.from_yolo_dataset(Path("verdis_category_dataset"))
+    dataloader.from_registry(VerdisBeltGroundTruth, _label_json_to_category_func=lambda x: x.category, file_to_saved_img_func=file_to_saved_img_func, prefix="verdis/gadstrup/5")
     image = dataloader.sample_image()
     print(image.shape)
 
-    trainer = YoloTrainer("verdis_category_reworked", CATEGORIES, dataloader=dataloader)
+    trainer = YoloTrainer("verdis_category_reworked2", CATEGORIES, dataloader=dataloader)
     trainer.train(epochs=10, patience=5, imgsz=224, batch=32)
 
     trainer.eval()
